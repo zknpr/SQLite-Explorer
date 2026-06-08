400a1ce64a9b57e8ebbe00edfa4c55cdb48f76ec
zknpr <zknpr_gh@protonmail.com>
2026-06-08 18:35:44 +0200

    chore: remove committed test_plan.ts scratch file

--- test_plan.ts
+++
@@ -1,4 +0,0 @@
-import { ModificationTracker } from "./src/core/undo-history";
-
-const tracker = new ModificationTracker<any>(1);
-console.log((tracker as any).getCheckpointInvalidationRevision());
